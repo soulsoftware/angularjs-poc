fa41dd307a5b668040e4ea23c707ac87de4b2961
bsorrentino <bartolomeo.sorrentino@gmail.com>
2015-03-11 19:30:26 +0100

    add $http support

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,22 @@
  * Controller of the angularjsPocApp
  */
 angular.module('angularjsPocApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http /* IOC */) {
 
     $scope.author = { "name":"Bartolomeo", "surname":"Sorrentino" };
+
+
+$http.get('http://192.168.1.111:3000').
+  success(function(data, status, headers, config) {
+    // this callback will be called asynchronously
+    // when the response is available
+    console.log( data );
+
+  }).
+  error(function(data, status, headers, config) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+
+    console.log( "ERROR!" );
+  });
 });
