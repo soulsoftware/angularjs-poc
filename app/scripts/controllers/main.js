e72342f9242a81217b6a5ff4ed06923e87337cd1
bsorrentino <bartolomeo.sorrentino@gmail.com>
2015-03-11 18:31:13 +0100

    add binding

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,6 @@
  */
 angular.module('angularjsPocApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+
+    $scope.author = { "name":"Bartolomeo", "surname":"Sorrentino" };
+});
